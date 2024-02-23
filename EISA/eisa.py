b6ce4318298a0f6e4221bc4365e6d016509b2efa
shankarchawla1776 <shankarchawla9101112@gmail.com>
2024-02-22 23:37:47 -0700

    word vec fix

--- EISA/eisa.py
+++ EISA/eisa.py
@@ -4,16 +4,18 @@ import openai
 from dotenv import load_dotenv
 from transformers import pipeline
 import sys
-from SideNet.side_net import SideNet
+# from SideNet.side_net import SideNet
 from gensim.test.utils import common_texts, common_dictionary, common_corpus
 from gensim.models import Word2Vec
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity 
+from word_vecs.word_vecs import model, vocab
 
 load_dotenv()
 OPEN_AI_API_KEY = os.getenv("OPEN_AI_API_KEY")
 openai.api_key = OPEN_AI_API_KEY
-
+# model = Word2Vec(common_texts, min_count=1)
+# vocab = set(model.wv.index_to_key)
 class E_I_S_A:
     def __init__(self, config={"api_key": OPEN_AI_API_KEY}) -> None:
         self.config = config
@@ -36,8 +38,7 @@ class E_I_S_A:
         return chat.choices[0].message.content
 
     def ENCM(self, input): 
-        model = Word2Vec(common_texts, min_count=1)
-        vocab = set(model.wv.index_to_key)
+
         spl = input.split()
         filt_mem = [word for word in spl if word in vocab]
 
@@ -49,36 +50,36 @@ class E_I_S_A:
                 vec = model.wv[word]
                 data.append([word] + vec.tolist())  
             def Search(test_episodes): 
-                sim = []
+                # sim = []
                 final_search = []
-                for db_entry in db: 
-                    for data_entry in data: 
-                        sim.append([abs(db_val - data_val) for db_val, data_val in zip(db_entry[1:], data_entry[1:])])
-                for k in sim: 
-                    if any(diff < 0.00005 for diff in k): 
-                        final_search.append(k)
-                if final_search:
-                    average_vector = np.mean(final_search, axis=0)
-                    most_similar_episode = None
-                    max_similarity_score = -1
+                # for db_entry in db: 
+                #     for data_entry in data: 
+                #         sim.append([abs(db_val - data_val) for db_val, data_val in zip(db_entry[1:], data_entry[1:])])
+                # for k in sim: 
+                #     if any(diff < 0.00005 for diff in k): 
+                #         final_search.append(k)
+                # if final_search:
+                average_vector = np.mean(final_search, axis=0)
+                most_similar_episode = None
+                max_similarity_score = -1
 
-                    for episode in test_episodes:
-                        spl_1 = episode.split()
-                        filt_epi = [word for word in spl_1 if word in vocab]
-                        episode_vector = np.mean([model.wv[word] for word in filt_epi], axis=0)
+                for episode in test_episodes:
+                    spl_1 = episode.split()
+                    filt_epi = [word for word in spl_1 if word in vocab]
+                    episode_vector = np.mean([model.wv[word] for word in filt_epi], axis=0)
 
-                    if not np.isnan(episode_vector).any():
+                if not np.isnan(episode_vector).any():
                             
-                        similarity_score = cosine_similarity([episode_vector], [average_vector])[0][0]
+                    similarity_score = cosine_similarity([episode_vector], [average_vector])[0][0]
 
-                        if similarity_score > max_similarity_score:
-                            most_similar_episode = episode
-                            max_similarity_score = similarity_score
+                    if similarity_score > max_similarity_score:
+                        most_similar_episode = episode
+                        max_similarity_score = similarity_score
 
                     return most_similar_episode
-                
-                else: 
-                    return None
+                return None
+                # else: 
+                #     return None
             return Search(test_episodes)
             
         need = None
---
+++ norm_mem.py
@@ -0,0 +1,12 @@
+from EISA.eisa import generated_response
+from gensim.test.utils import common_texts, common_dictionary, common_corpus
+from gensim.models import Word2Vec
+
+model = Word2Vec(common_texts, min_count=1)
+vocab = set(model.wv.index_to_key)
+
+spl = generated_response.split()
+filt_mem = [word for word in spl if word in vocab]
+
+print(generated_response)
+
